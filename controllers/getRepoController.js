07b9ab1402214475168e0ad60306239c1ff2f081
GavinThomas1192 <gthomas1192@gmail.com>
2017-11-14 09:11:55 -0800

    promisfy

--- controllers/getRepoController.js
+++ controllers/getRepoController.js
@@ -1,14 +1,2 @@
 'use strict';
-var app = app || {};
 
-(function (module) {
-    const getReposController = {};
-
-    getReposController.init = function () {
-
-        app.repos.requestRepos(app.repoView.index);
-        app.repos.requestUser(app.repoView.index);
-    }
-
-    module.getReposController = getReposController;
-})(app);
